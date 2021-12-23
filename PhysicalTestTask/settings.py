1fc715f6899fbbf5ac1167e3bd3bc1e54b6eab46
Danil <far.danil@outlook.com>
2021-12-23 17:29:14 +0300

    deploy fixies

--- PhysicalTestTask/settings.py
+++ PhysicalTestTask/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, '/static/')
 STATICFILES_DIRS = (
     ("js", os.path.join(STATIC_ROOT, 'js')),
     ("css", os.path.join(STATIC_ROOT, 'css')),
