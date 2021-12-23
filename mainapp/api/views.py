0ddd6cb3a1cd8883d24c2d0c7e9ddb8c2217b33a
Danil <far.danil@outlook.com>
2021-12-23 20:45:43 +0300

    code refactoring

--- mainapp/api/views.py
+++ mainapp/api/views.py
@@ -1,17 +1,29 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from ..utils.week_counter import get_week_number
+from mainapp.utils.week_counter import get_week_number
 
 
 class WeekCounterView(APIView):
     def get(self, request):
-        if request.query_params.get('date'):
-            day, month, year = map(int, request.query_params.get('date').split('_'))
+        """
+
+        Args:
+            request: The request that is being processed
+
+        Returns:
+            Response: dict (json) in format {"week": (int)} if success, else {"error": "(str)"}
+
+        """
+        if date := request.query_params.get('date'):
+            day, month, year = map(int, date.split('_'))
             try:
                 weeks_amount = get_week_number(day, month, year)
             except ValueError:
+                # catching incorrect date format
                 return Response({'error': 'incorrect date'}, status=400, exception=True)
             else:
+                # successful response
                 return Response({'week': weeks_amount})
         else:
+            # catching empty or invalid query params
             return Response({'error': 'empty or not allowed query params'}, status=400, exception=True)
--- mainapp/utils/week_counter.py
+++ mainapp/utils/week_counter.py
@@ -2,14 +2,18 @@ from datetime import datetime
 from django.conf import settings
 
 
-def get_week_number(day: int, month: int, year: int) -> int:
-    current_date = datetime(year=year, month=month, day=day)
-    delta = current_date - settings.START_DATE
+def get_week_number(day, month, year):
+    """
+        Args:
+            day (int): days value
+            month (int): months value
+            year (int): years value
+
+        Returns:
+            int: number of week
+
+    """
+    current_date = datetime(year=year, month=month, day=day)  # creating datetime obj
+    delta = current_date - settings.START_DATE  # calculating difference
+
     return delta.days // 7 + 1
-
-
-
-
-
-
-
